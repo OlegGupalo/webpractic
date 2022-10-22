423f4239eede3ced63fcdb18ba42f0b3b0b3036f
OlegGupalo <gupaloo26@gmail.com>
2022-10-22 14:31:36 +0300

    fix-bugs

--- components/question/Question.tsx
+++ components/question/Question.tsx
@@ -5,47 +5,76 @@ import styles from './Question.module.scss'
 export const Question = () => {
     return (
         <Box sx={{
-            backgroundColor: '#E8E9EB',
-            margin: '20px 0px'
+            backgroundImage: 'linear-gradient(to right, #ed6ea0, #ec8c69, #f7186a , #FBB03B)',
+            boxShadow: '0 4px 15px 0 rgba(236, 116, 149, 0.75)',
+            margin: '75px 0px 0px 0px',
+            borderRadius: '80px',
+            paddingBottom: '20px'
         }}>
             <Typography variant="h1" classes={{root: styles.questionTitle}}>
                 Задать вопрос ректору
             </Typography>
-                <Box>
-                    <Grid container>
-                        <Grid item xs={6}>
+                <Box >
+                    <Grid container sx={{
+                        display: 'flex',
+                        justifyContent: 'space-evenly'
+                    }}>
                             <TextField 
                                 placeholder="Имя"
-                                fullWidth
+                                classes={{root: styles.defaultInput}}
                                 inputProps={{sx: {
-                                    fontFamily: 'Montserrat, sans-serif'
+                                    width: '420px',
+                                    fontFamily: 'Montserrat, sans-serif',
+                                    border: '1px solid wheat',
+                                    color: 'black',
+                                    fontWeight: '700',
+                                    fontSize: '20px',
+                                    backgroundColor: 'wheat',
+                                    borderRadius: '20px',
                                 }}}
                             />
-                        </Grid>
-                        <Grid item xs={6}>
                             <TextField 
                                 placeholder="E-mail"
-                                fullWidth
                                 inputProps={{sx: {
-                                    fontFamily: 'Montserrat, sans-serif'
+                                    width: '420px',
+                                    fontFamily: 'Montserrat, sans-serif',
+                                    border: '1px solid wheat',
+                                    color: 'black',
+                                    fontWeight: '700',
+                                    fontSize: '20px',
+                                    backgroundColor: 'wheat',
+                                    borderRadius: '20px',
                                 }}} 
                             />
-                        </Grid>
                     </Grid>
-                    <TextField
-                        placeholder="Ваше сообщение"
-                        multiline
-                        maxRows={4}
-                        fullWidth
-                        InputProps={{sx: {
-                            height: 140,
-                            fontSize: 20,
-                            fontFamily: 'Montserrat, sans-serif',
-                            letterSpacing: '1.5px',
-                            margin: '15px 0px'
-                        }}}
-                    />
+                    <Box sx={{
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                        <TextField
+                            placeholder="Ваше сообщение"
+                            multiline
+                            maxRows={4}
+                            InputProps={{sx: {
+                                marginTop: '15px',
+                                height: 140,
+                                fontSize: 20,
+                                fontFamily: 'Montserrat, sans-serif',
+                                letterSpacing: '1.5px',
+                                margin: '15px 0px',
+                                width: '968px',
+                                border: '1px solid wheat',
+                                color: 'black',
+                                fontWeight: '700',
+                                backgroundColor: 'wheat',
+                                borderRadius: '20px',
+                            }}}
+                        />
+                    </Box>
+                    
                     <Button
+                        classes={{root: styles.button}}
+                        size="large"
                         variant="contained"
                     >Задать вопрос</Button>
                     
---
+++ pages/profile.tsx
@@ -0,0 +1,12 @@
+import { UserProfile } from "components/profile/UserProfile"
+import { MainLayout } from "layouts/MainLayout"
+
+const Profile = () => {
+    return (
+        <MainLayout>
+            <UserProfile />
+        </MainLayout>
+    )
+}
+
+export default Profile
